6b3558793f3d3a986224671ad3c2cc5a801f1613
Ruqia Kassaw Mengistu <ruqiakassaw48@gmail.com>
2026-06-08 11:00:43 +0300

    Add solution for problem 1791C

---
+++ _1791C.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    s = input().strip()
+
+    l, r = 0, n - 1
+
+    while l < r and s[l] != s[r]:
+        l += 1
+        r -= 1
+
+    print(r - l + 1)
