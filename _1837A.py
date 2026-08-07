ebf5616294ad682d14ff89ab6300feb225fccb6c
Ruqia Kassaw Mengistu <ruqiakassaw48@gmail.com>
2026-08-07 09:50:32 +0300

    Implement logic to handle divisibility checks

---
+++ _1837A.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for i in range(t):
+    x, k = map(int, input().split())
+
+    if x % k != 0:
+        print(1)
+        print(x)
+    else:
+        if (x - 1) % k != 0:
+            print(2)
+            print(x - 1, 1)
+        else:
+            print(2)
+            print(x + 1, -1)
