95c05a74873b330a3a845911d8e651deb70f26d9
Ruqia Kassaw Mengistu <ruqiakassaw48@gmail.com>
2026-08-23 14:06:11 +0300

    Implement dragon game logic in _230A.py

---
+++ _230A.py
@@ -0,0 +1,17 @@
+s, n = map(int, input().split())
+
+dragons = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    dragons.append((x, y))
+
+dragons.sort()
+
+for x, y in dragons:
+    if s <= x:
+        print("NO")
+        break
+    s += y
+else:
+    print("YES")
